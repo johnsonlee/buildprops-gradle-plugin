4cd4d9b45afd8161dcc7d9e963cab54f08f95f35
johnsonlee <g.johnsonlee@gmail.com>
2021-01-01 14:12:10 +0800

    Support plugins DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
-    kotlin("jvm") version embeddedKotlinVersion
-    kotlin("kapt") version embeddedKotlinVersion
     `java-library`
+    `java-gradle-plugin`
     `signing`
 
-    id("io.codearte.nexus-staging") version "0.21.2"
+    id("com.gradle.plugin-publish") version "0.12.0"
     id("de.marcphilipp.nexus-publish") version "0.4.0"
+    id("io.codearte.nexus-staging") version "0.21.2"
+
+    kotlin("jvm") version embeddedKotlinVersion
+    kotlin("kapt") version embeddedKotlinVersion
 }
 
 group = "io.johnsonlee.buildprops"
 version = "1.0.0"
+description = "Generate Build.java for module"
 
 
 repositories {
@@ -43,6 +47,36 @@ val javadocJar by tasks.registering(Jar::class) {
 val OSSRH_USERNAME = project.properties["OSSRH_USERNAME"] as? String ?: System.getenv("OSSRH_USERNAME")
 val OSSRH_PASSWORD = project.properties["OSSRH_PASSWORD"] as? String ?: System.getenv("OSSRH_PASSWORD")
 
+gradlePlugin {
+    plugins {
+        create("buildPropsPlugin") {
+            id = "io.johnsonlee.buildprops"
+            implementationClass = "io.johnsonlee.buildprops.BuildPropsPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/johnsonlee/${project.name}"
+    vcsUrl = "https://github.com/johnsonlee/${project.name}"
+    description = project.description
+
+    (plugins) {
+        "buildPropsPlugin" {
+            displayName = project.name
+            description = project.description
+            tags = listOf("build", "gradle", "kotlin")
+            version = "${project.version}"
+        }
+    }
+
+    mavenCoordinates {
+        groupId = "${project.group}"
+        artifactId = project.name
+        version = "${project.version}"
+    }
+}
+
 nexusStaging {
     packageGroup = "io.johnsonlee"
     username = OSSRH_USERNAME
@@ -60,6 +94,7 @@ nexusPublishing {
     }
 }
 
+
 publishing {
     repositories {
         maven {
@@ -67,12 +102,15 @@ publishing {
         }
     }
     publications {
-        register("mavenJava", MavenPublication::class) {
+        withType(MavenPublication::class.java).configureEach {
+            val publication = this
             groupId = "${project.group}"
             artifactId = project.name
             version = "${project.version}"
 
-            from(components["java"])
+            if ("mavenJava" == publication.name) {
+                from(components["java"])
+            }
 
             artifact(sourcesJar.get())
             artifact(javadocJar.get())
@@ -102,10 +140,10 @@ publishing {
                     }
                 }
             }
+
+            project.signing {
+                sign(publication)
+            }
         }
     }
-}
-
-signing {
-    sign(publishing.publications["mavenJava"])
 }
